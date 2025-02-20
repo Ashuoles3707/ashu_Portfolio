23d99b6cd886fe15d9bbb6289294650d57dccb25
Ashutosh Olekar <163170042+Ashuoles3707@users.noreply.github.com>
2025-02-20 16:31:19 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,147 @@
+const logotext = "Mr. Ashu";
+const meta = {
+    title: "Mr. Ashu Olekar",
+    description: "I’m Mr. Ashu Olekar data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Ashutosh Olekar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop websites",
+    },
+    description: "I’m Full Stack Developer",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "2+ years of experience in full-stack PHP development, with proven expertise in creating dynamic and scalable web applications. Contributed to multiple projects, demonstrating strong problem-solving and coding abilities.Currently advancing my knowledge in full-stack Java technologies, including Spring Boot and Spring MVC,JavaScript, React, and SQL, at I.T. Vedant to expand my technical proficiency and adapt to modern industry requirements.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developer",
+        where: "Orient Consultancy",
+        date: "Nov-2022",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Spring Boot",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Sql",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Java Developer",
+        description: "A Java Developer is responsible for designing, developing, and maintaining applications using the Java programming language.",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ashutosh.balaso.olekar@gmail.com",
+    YOUR_FONE: "(+91)9503322604",
+    description: "Address : Pune,Maharashtra,India ",
+    YOUR_SERVICE_ID: "service_nd802cz",
+    YOUR_TEMPLATE_ID: "template_f1jzixv",
+    YOUR_USER_ID: "D0aiwFxip0423g0t4",
+};
+
+const socialprofils = {
+    github: "https://github.com/Ashuoles3707",
+    facebook: "https://www.facebook.com/ashutosh.olekar/?locale=en_GB&_rdr",
+    linkedin: "https://www.linkedin.com/in/ashutosh-olekar-373360219/",
+    twitter: "https://x.com/ashu_olekar2604",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
